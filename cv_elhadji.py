7f6331139c5e8d4645b1326da747bdd22e415091
elhadjidieganegning-ship-it <elhadjidieganegning@gmail.com>
2026-04-17 16:15:56 +0000

    Add files via upload

---
+++ cv_elhadji.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+#configuration de la page
+st.set_page_config(page_title="cv - Elhadji Diegane Gning",page_icon="photo.pjg",layout="centered")
+
+# titre
+st.title("ELHADJI DIEGANE GNING")
+
+
+st.markdown("---")
+
+# profils
+st.write("Etudiant motive en Lience 2 Geographie et en premiere annee de BTS en Geomatique, "
+         "passionne par les systemes information geographique (SIG), la cartographie "
+         " analyse spatiale dessin plan et programmation debutant.")
+
+# formations
+st.header(" formation et diplomes")
+st.write("**2025/2026 LIENCE EN GEOGRAPHIE A UNIVERSITE CHEIKH ANTA DIOP**")
+
+st.write("**2025/2026 BREVRET TECHNICIEN SUPERIEUR EN GEOMATIQUE AU CENTRE D'ENTREPRENEURIAT ET DE DEVELOPPEMENT TECHNIQUE LE G15**")
+
+st.write("**2022/2023 BACCALAUREAT A UNIVERSITE CHEIKH ANTA DIOP**")
+
+# competences
+st.header(" competences")
+st.markdown(""" 
+- Outils informatique( suite Office )
+- Cartographie (QGIS)
+- SIG (QGIS, notions ARCGIS)
+- Analyse Sptiale
+- Python
+- lecture et interpretation de carte
+- dessin plan (AUTOCAD ET SKETCHUP)
+- topographies
+- collectes de donnees
+- teledections
+- analyses documentaires
+- pilote de drone 
+""")
+
+with st.sidebar:    
+    # Information personnelles
+    st.write("ELHADJI DIEGANE GNING")
+    st. write("**TECHNICIEN SUPERIEUR EN GEOMATIQUE ET GEOGRAPHE**")
+    st.write(" EMAIL:elhadjidieganegning@gmail.com")
+    st.write(" TELEPHONE : 77 797 47 14")
+    st.write(" Adersse : Fatick;SENEGAL")
+        
+    
+# Pied de page
+st.markdown("---")
+st.write(" 2026 - ELHADJI DIEGANE GNING")
